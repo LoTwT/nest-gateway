ed2fd87c679b08d233d50beb8155b8dedfb5378a
LoTwT <709937065@qq.com>
2022-08-02 22:35:59 +0800

    feat: catch global error exception

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import {
   NestFastifyApplication,
 } from "@nestjs/platform-fastify"
 import { TransformInterceptor } from "./common/interceptors/transform.interceptor"
+import { AllExceptionsFilter } from "./common/exceptions/base.exception.filter"
+import { HttpExceptionFilter } from "./common/exceptions/http.exception.filter"
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -14,6 +16,10 @@ async function bootstrap() {
 
   // 统一响应体格式
   app.useGlobalInterceptors(new TransformInterceptor())
+
+  // 异常过滤器
+  // 注意引入自定义异常的先后顺序，不然异常捕获逻辑可能出现混乱
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter())
 
   await app.listen(3000)
 }
