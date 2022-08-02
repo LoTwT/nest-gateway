7ab209292e5b03423c9f9e192be248ec5e7b0e0b
LoTwT <709937065@qq.com>
2022-08-02 20:43:01 +0800

    feat: response interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify"
+import { TransformInterceptor } from "./common/interceptors/transform.interceptor"
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   )
+
+  // 统一响应体格式
+  app.useGlobalInterceptors(new TransformInterceptor())
+
   await app.listen(3000)
 }
 bootstrap()
