02929eec12777b4c6a1a22f6171f891fc43ec8da
LoTwT <709937065@qq.com>
2022-08-25 15:41:58 +0800

    feat: feishu api

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,8 +8,21 @@ export const getEnv = () => process.env.RUNNING_ENV
 // 读取项目配置
 export const getConfig = () => {
   const environment = getEnv()
-  const yamlPath = path.join(process.cwd(), `./.config/.${environment}.yaml`)
-  const file = fs.readFileSync(yamlPath, "utf-8")
+
+  let file
+
+  try {
+    file = fs.readFileSync(
+      path.join(process.cwd(), `./.config/.${environment}.local.yaml`),
+      "utf-8",
+    )
+  } catch (error) {
+    file = fs.readFileSync(
+      path.join(process.cwd(), `./.config/.${environment}.yaml`),
+      "utf-8",
+    )
+  }
+
   const config = parse(file)
 
   return config
